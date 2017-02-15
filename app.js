166321ddc5c7df4f17e41036a1b4e0bdc6642564
Michael Carniato <mcarniato1991@gmail.com>
2017-02-15 14:36:01 +1100

    add static folder

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(__dirname + '/static'));
+
+//View Engine
+app.set('view engine', 'ejs');
+
 
 app.get('/', function(req, res) {
-  res.send('Hello!');
+  res.render('index');
 });
 
 app.listen(8080, function() {
