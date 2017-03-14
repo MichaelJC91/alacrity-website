b74c1f2d1a3a4fb3043635ba4338893033a96a99
Michael Carniato <Michael Carniato>
2017-03-14 15:03:14 +1100

    modified expiry headers

--- app.js
+++ app.js
@@ -30,11 +30,8 @@ app.set('view engine', 'ejs');
 
 //Set expiry headers
 app.get('/*', function (req, res, next) {
-
-  if (req.url.indexOf("/images/") === 0 || req.url.indexOf("/stylesheets/") === 0) {
-    res.setHeader("Cache-Control", "public, max-age=2592000");
-    res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
-  }
+  res.setHeader("Cache-Control", "public, max-age=2592000");
+  res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
   next();
 });
 
