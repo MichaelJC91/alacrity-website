7f1678c7df3b05ac66f708252c6ae008deaf0ec4
Michael Carniato <mcarniato1991@gmail.com>
2017-05-04 16:16:38 +1000

    changed google maps height

--- static/js/columnHeights.js
+++ static/js/columnHeights.js
@@ -1,5 +1,5 @@
 //Get height of 1st column
 export default function colHeight() {
 let colHeight = $('.contactFirstCol').height();
-$('#googleMap').css('height', colHeight + 'px');
+$('.googleMapWrapper').css('height', colHeight + 'px');
 }
